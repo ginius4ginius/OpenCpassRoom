6de0307e880611675d0b6091197d6b790f826298
ginius4ginius <derieu.vincent@outlook.com>
2017-12-14 05:38:23 +0100

    maj

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <?php include("vues/head.php"); ?>
+    <body>
+        <?php
+
+        ?>
+    </body>
+  <?php include("vues/foot.php"); ?>
+</html>
---
+++ vues/foot.php
@@ -0,0 +1,3 @@
+<footer id="pied_de_page">
+        <p>Copyright en-CDI6-2017/2018, tous droits réservés</p>
+    </footer>
